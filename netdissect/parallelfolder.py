04808a4796d8ef974cd3615dab5b31cf73409682
David Bau <davidbau@csail.mit.edu>
2020-12-03 14:26:45 -0500

    Add subset feature.

--- netdissect/parallelfolder.py
+++ netdissect/parallelfolder.py
@@ -5,10 +5,12 @@ cached files with lists of filenames, etc.
 '''
 
 import os
+import torch
 import re
 import random
 import numpy
 import itertools
+import copy
 import torch.utils.data as data
 from torchvision.datasets.folder import default_loader as tv_default_loader
 from PIL import Image
@@ -33,9 +35,9 @@ def default_loader(filename):
     '''
     Handles both numpy files and image formats.
     '''
-    if filename.endswith('.npy'):
+    if filename.endswith('.npy') or filename.endswith('.NPY'):
         return numpy.load(filename).view(ndarray)
-    elif filename.endswith('.npz'):
+    elif filename.endswith('.npz') or filename.endswith('.NPZ'):
         return numpy.load(filename)
     else:
         return tv_default_loader(filename)
@@ -59,6 +61,7 @@ class ParallelImageFolders(data.Dataset):
                  loader=default_loader,
                  stacker=None,
                  classification=False,
+                 identification=False,
                  intersection=False,
                  filter_tuples=None,
                  normalize_filename=None,
@@ -72,6 +75,7 @@ class ParallelImageFolders(data.Dataset):
         self.transforms = transform
         self.stacker = stacker
         self.loader = loader
+        self.identification = identification
 
         def do_lazy_init():
             self.images, self.classes, self.class_to_idx = (
@@ -93,6 +97,29 @@ class ParallelImageFolders(data.Dataset):
             self._do_lazy_init = do_lazy_init
         else:
             do_lazy_init()
+
+    def subset(self, indexes):
+        '''
+        Returns a subset of the current dataset, given by
+        the set of specified indexes.
+        '''
+        if self._do_lazy_init is not None:
+            self._do_lazy_init()
+        # Copy over transforms and other settings.
+        ds = ParallelImageFolders(
+            self.image_roots,
+            transform=self.transforms,
+            loader=self.loader,
+            stacker=self.stacker,
+            identification=self.identification,
+            lazy_init=True)
+        # Initialize the subset items directly.
+        ds.images = [
+            copy.deepcopy(self.images[i]) for i in indexes]
+        ds.classes = self.classes
+        ds.class_to_idx = self.class_to_idx
+        ds._do_lazy_init = None
+        return ds
 
     def __getattr__(self, attr):
         if self._do_lazy_init is not None:
@@ -122,12 +149,15 @@ class ParallelImageFolders(data.Dataset):
                        in itertools.zip_longest(sources, self.transforms)]
         if self.stacker is not None:
             sources = self.stacker(sources)
-            if self.classes is not None:
-                sources = (sources, classidx)
-        else:
-            if self.classes is not None:
-                sources.append(classidx)
-            sources = tuple(sources)
+            if self.classes is None and not self.identification:
+                return sources
+            else:
+                sources = [sources]
+        if self.classes is not None:
+            sources.append(classidx)
+        if self.identification:
+            sources.append(index)
+        sources = tuple(sources)
         return sources
 
     def __len__(self):
@@ -137,7 +167,8 @@ class ParallelImageFolders(data.Dataset):
 
 
 def is_npy_file(path):
-    return path.endswith('.npy') or path.endswith('.NPY')
+    return (path.endswith('.npy') or path.endswith('.NPY') or
+            path.endswith('.npz') or path.endswith('.NPZ'))
 
 
 def is_image_file(path):
@@ -153,8 +184,9 @@ def walk_image_files(rootdir, verbose=None):
                              for line in f.readlines()])
             return result
     result = []
-    for dirname, _, fnames in sorted(pbar(os.walk(rootdir),
-                                          desc='Walking %s' % os.path.basename(rootdir))):
+    # for dirname, _, fnames in sorted(pbar(os.walk(rootdir),
+    #         desc='Walking %s' % os.path.basename(rootdir))):
+    for dirname, _, fnames in sorted(os.walk(rootdir)):
         for fname in sorted(fnames):
             if is_image_file(fname) or is_npy_file(fname):
                 result.append(os.path.join(dirname, fname))
@@ -165,7 +197,7 @@ def make_parallel_dataset(image_roots, classification=False,
                           intersection=False, filter_tuples=None, normalize_fn=None,
                           verbose=None):
     """
-    Returns ([(img1, img2, clsid), (img1, img2, clsid)..],
+    Returns ([(img1, img2, clsid, id), (img1, img2, clsid, id)..],
              classes, class_to_idx)
     """
     image_roots = [os.path.expanduser(d) for d in image_roots]
@@ -178,8 +210,8 @@ def make_parallel_dataset(image_roots, classification=False,
             if key not in image_sets:
                 image_sets[key] = []
             if not intersection and len(image_sets[key]) != j:
-                raise RuntimeError(
-                    'Images not parallel: %s missing from one dir' % (key))
+                raise RuntimeError('Images not parallel: '
+                                   '{} missing from {}'.format(key, root))
             image_sets[key].append(path)
     if classification:
         classes = sorted(set([os.path.basename(os.path.dirname(k))
@@ -191,7 +223,7 @@ def make_parallel_dataset(image_roots, classification=False,
         classes, class_to_idx = None, None
     tuples = []
     for key, value in image_sets.items():
-        if len(value) != len(image_roots) + (1 if classification else 0):
+        if len(value) != (len(image_roots) + (1 if classification else 0)):
             if intersection:
                 continue
             else:
@@ -202,3 +234,18 @@ def make_parallel_dataset(image_roots, classification=False,
             continue
         tuples.append(value)
     return tuples, classes, class_to_idx
+
+
+class NpzToTensor:
+    """
+    A data transformer for converting a loaded npz file to a pytorch
+    tensor.  Since an npz file stores tensors under keys, a key can be
+    specified.  Otherwise, the first key is dereferenced.
+    """
+
+    def __init__(self, key=None):
+        self.key = key
+
+    def __call__(self, data):
+        key = self.key or next(iter(data))
+        return torch.from_numpy(data[key])
